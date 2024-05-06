d2d1f839c39d62ccc00a25e984103f60fd0a6022
Weslen Lakins <weslen.lakins@outlook.com>
2024-05-05 20:28:59 -0400

    chore: Refactor server.js to improve API routing and error handling

--- server.js
+++ server.js
@@ -17,38 +17,32 @@ app.get("/api/easteregg", function (req, res) {
 
 app.get("/api/timestamp", function (req, res) {
 	const date = new Date();
-
-	res.json({
-		unix: date.valueOf(),
-		utc: date.toUTCString(),
-	});
+	res.json({ unix: date.valueOf(), utc: date.toUTCString() });
 });
 
 app.get("/api/timestamp/:dateParam", function (req, res) {
 	let dateParam = req.params.dateParam;
-
 	if (/^\d{5,}$/.test(dateParam)) dateParam = parseInt(dateParam);
 
 	const date = new Date(dateParam);
-
 	if (date.toString() == "Invalid Date") {
-		res.json({
-			error: "Invalid Date",
-		});
+		res.json({ error: "Invalid Date" });
 	} else {
-		res.json({
-			unix: date.valueOf(),
-			utc: date.toUTCString(),
-		});
+		res.json({ unix: date.valueOf(), utc: date.toUTCString() });
 	}
 });
 
-// No matching route
+// Catch-all route to handle client-side routing
+app.get("*", function (req, res) {
+	res.sendFile(`${__dirname}/views/index.html`);
+});
+
+// No matching API route
 app.use(function (req, res, next) {
 	res.status(404).sendFile(`${__dirname}/views/404.html`);
 });
 
 // Listening for requests
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
 	console.log("Your app is listening on port " + listener.address().port);
 });
