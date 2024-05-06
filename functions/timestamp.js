cd1b4b09988b50c085f6e876a82da4a35d7c8e8c
Weslen Lakins <weslen.lakins@outlook.com>
2024-05-06 13:43:15 -0400

    chore: Refactor timestamp API endpoints, improve routing, and handle EST timestamps

--- functions/timestamp.js
+++ functions/timestamp.js
@@ -3,25 +3,6 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 
-// Helper function to format a date in GMT
-function formatDateToGMT(date) {
-	return date.toUTCString();
-}
-
-// Helper function to format a date in EST
-function formatDateToEST(date) {
-	return new Intl.DateTimeFormat("en-US", {
-		timeZone: "America/New_York",
-		year: "numeric",
-		month: "2-digit",
-		day: "2-digit",
-		hour: "2-digit",
-		minute: "2-digit",
-		second: "2-digit",
-		hour12: false,
-	}).format(date);
-}
-
 // Current timestamp or specific date timestamp endpoint
 router.get("/timestamp/:dateParam?", (req, res) => {
 	const { dateParam } = req.params;
@@ -44,8 +25,7 @@ router.get("/timestamp/:dateParam?", (req, res) => {
 	} else {
 		res.json({
 			unix: date.getTime(),
-			utc: formatDateToGMT(date),
-			est: formatDateToEST(date),
+			utc: date.toUTCString(),
 		});
 	}
 });
@@ -55,7 +35,6 @@ router.get("/easteregg", (req, res) => {
 	res.json({ greeting: "Oh, you've found this! Well, congrats! :p" });
 });
 
-// Handle all routes starting with "/api"
 app.use("/api", router);
 
 module.exports.handler = serverless(app);
